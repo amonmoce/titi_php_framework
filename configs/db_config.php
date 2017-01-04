f58c876c9a010278c64a8028fd53ffc0d41a071b
amon <amon@gs8899.com>
2017-01-04 09:31:27 +0800

    v0.0.0.1

--- configs/db_config.php
+++ configs/db_config.php
@@ -1,6 +1,6 @@
 <?php
-$linki = mysqli_connect('localhost', 'root', '0000');
-mysqli_select_db($linki,'tiehua') or die('1234');
+$linki = mysqli_connect('10.10.137.227', 'bond', 'TsOy_1215');
+mysqli_select_db($linki,'bond') or die('1234');
 mysqli_query($linki,'SET NAMES utf8');
 mysqli_query($linki,'SET CHARACTER_SET_CLIENT=utf8');
 mysqli_query($linki,'SET CHARACTER_SET_RESULTS=utf8');
