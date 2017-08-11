0ed3b50cbde9c4e4b2f1c348988a2c138f17d0b2
Amon <amonmoce@apuremail.com>
2017-08-11 16:55:25 +0800

    added css to table and jquery tablesorter

--- templates/index.html.php
+++ templates/index.html.php
@@ -1,23 +1,27 @@
-<table>
-  <tr>
-    <th>Name</th>
-    <th>Cel</th> 
-    <th>Code</th>
-    <th>Time</th>
-    <th>Store</th>
-  </tr>
-<?php if(isset($this->data)){ 
-    foreach ($this->data['users'] as $key => $value) {
-?>
-<tr>
-<td><?php echo $value["name"]; ?></td>
-<td><?php echo $value["cel"]; ?></td> 
-<td><?php echo $value["code"]; ?></td>
-<td><?php echo $value["time"]; ?></td> 
-<td><?php echo $value["store"]; ?></td>
-</tr>
+<h1>Event data</h1>
+<table id="keywords" cellspacing="0" cellpadding="0">
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Cel</th> 
+      <th>Code</th>
+      <th>Time</th>
+      <th>Store</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php if(isset($this->data)){ 
+        foreach ($this->data['users'] as $key => $value) {
+    ?>
+    <tr>
+    <td><?php echo $value["name"]; ?></td>
+    <td><?php echo $value["cel"]; ?></td> 
+    <td><?php echo $value["code"]; ?></td>
+    <td><?php echo $value["time"]; ?></td> 
+    <td><?php echo $value["store"]; ?></td>
+    </tr>
 
-<?php } } ?>
+    <?php } } ?>
 
-  
+  </tbody>
 </table>
--- templates/layout.html.php
+++ templates/layout.html.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <title></title>
+        <link rel="stylesheet" href="templates/css/results_table.css">
     </head>
     <body>
         <?php
@@ -14,4 +15,7 @@
             }
         ?>
     </body>
+    <script type="text/javascript" src="templates/js/jquery-latest.js"></script>
+    <script type="text/javascript" src="templates/js/jquery.tablesorter.min.js"></script>
+    <script type="text/javascript" src="templates/js/results_table.js"></script>
 </html>
